24f0c79b6c02a961803177e47ba29c38f3c64783
Gustavo Santos <gustavo.st971@gmail.com>
2024-10-18 09:12:25 -0300

    docs: add documentation for Show Openings

--- handler/showOpening.go
+++ handler/showOpening.go
@@ -8,6 +8,19 @@ import (
 	"github.com/stringGustavo/gopportunities.git/schemas"
 )
 
+// @BasePath /api/v1
+
+// @Summary Show opening
+// @Description Show an existing job opening
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Param id query string true "Opening identification"
+// @Success 200 {object} ShowOpeningResponse
+// @Failure 400 {object} ErrorResponse
+// @Failure 404 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [get]
 func ShowOpeningHandler(ctx *gin.Context) {
 	id := ctx.Query("id")
 	if id == "" {
