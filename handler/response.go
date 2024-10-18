5182ebb9475108b6b6e39de726a558264ef97c12
Gustavo Santos <gustavo.st971@gmail.com>
2024-10-18 08:32:02 -0300

    feat: add swagger config

--- handler/response.go
+++ handler/response.go
@@ -16,7 +16,7 @@ func sendError(ctx *gin.Context, code int, msg string) {
 
 func sendSuccess(ctx *gin.Context, operation string, data interface{}) {
 	ctx.JSON(http.StatusOK, gin.H{
-		"message": fmt.Sprintf("operation from handler %s successfull", operation),
+		"message": fmt.Sprintf("operation from handler: '%s' successfull", operation),
 		"data":    data,
 	})
 }
--- router/routes.go
+++ router/routes.go
@@ -3,12 +3,20 @@ package router
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/stringGustavo/gopportunities.git/handler"
+	docs "github.com/stringGustavo/gopportunities.git/docs"
+	swaggerfiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
+
 )
 
 func initializeRoutes(router *gin.Engine) {
 	// Initialize routes
 	handler.InitializeHandler()
-	v1 := router.Group("/api/v1")
+
+	basePath := "/api/v1"
+	docs.SwaggerInfo.BasePath = basePath
+
+	v1 := router.Group(basePath)
 	{
 		v1.GET("/opening", handler.ShowOpeningHandler)
 		v1.POST("/opening", handler.CreateOpeningHandler)
@@ -16,4 +24,6 @@ func initializeRoutes(router *gin.Engine) {
 		v1.PUT("/opening", handler.UpdateOpeningHandler)
 		v1.GET("/openings", handler.ShowAllOpeningsHandler)
 	}
+	// Swagger docs
+	router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 }
