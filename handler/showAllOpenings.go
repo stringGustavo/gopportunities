4870ff5b50562a0d572901642707cc09636d8074
Gustavo Santos <gustavo.st971@gmail.com>
2024-10-18 08:30:29 -0300

    feat: add show all openings handler logic

--- handler/showAllOpenings.go
+++ handler/showAllOpenings.go
@@ -4,10 +4,16 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stringGustavo/gopportunities.git/schemas"
 )
 
 func ShowAllOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Openings",
-	})
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+
+	sendSuccess(ctx, "show-all-openings", openings)
 }
