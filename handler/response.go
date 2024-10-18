04bf64f9a9e471f0d19b1e3479ce9b441a5e28a0
Gustavo Santos <gustavo.st971@gmail.com>
2024-10-18 09:24:26 -0300

    docs: objects for documentation

--- handler/response.go
+++ handler/response.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/stringGustavo/gopportunities.git/schemas"
 )
 
 func sendError(ctx *gin.Context, code int, msg string) {
@@ -19,4 +20,34 @@ func sendSuccess(ctx *gin.Context, operation string, data interface{}) {
 		"message": fmt.Sprintf("operation from handler: '%s' successfull", operation),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode int    `json:"errorCode"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type DeleteOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type ShowOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
+}
+
+type ShowAllOpeningsResponse struct {
+	Message string                  `json:"message"`
+	Data    []schemas.OpeningResponse `json:"data"`
+}
+
+type UpdateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
